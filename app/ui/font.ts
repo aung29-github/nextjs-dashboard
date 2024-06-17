05dc23ef8303caff62dc5470b3a342e00c2ed244
aung29-github <aungkohtet2989@gmail.com>
2024-06-18 08:24:49 +0900

    updalod

---
+++ app/ui/font.ts
@@ -0,0 +1,11 @@
+import {Inter} from 'next/font/google';
+import { Poppins } from 'next/font/google';
+
+// inter
+export const inter = Inter({subsets : ['latin']});
+
+// poppin
+export const poppins = Poppins({
+    weight : ['500', '700', '400'],
+    subsets : ['latin']
+})

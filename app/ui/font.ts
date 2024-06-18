1b1067bab09567f5ec79c754c1ddfdfc1187b67a
aung29-github <aungkohtet2989@gmail.com>
2024-06-18 11:49:36 +0900

    update

--- app/ui/font.ts
+++ app/ui/font.ts
@@ -1,11 +1,15 @@
-import {Inter} from 'next/font/google';
+import {Inter, Lusitana} from 'next/font/google';
 import { Poppins } from 'next/font/google';
-
 // inter
 export const inter = Inter({subsets : ['latin']});
 
 // poppin
 export const poppins = Poppins({
     weight : ['500', '700', '400'],
+    subsets : ['latin']
+})
+
+export const lusitana = Lusitana({
+    weight : ['400', '700'],
     subsets : ['latin']
 })
